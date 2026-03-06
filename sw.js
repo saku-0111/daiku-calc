e8aea875f1d50e0eedd53acd64ce76dba8ebb649
saku-0111 <yusaku1379@gmail.com>
2026-03-06 21:48:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'daiku-calc-v1';
+const urlsToCache = [
+  '大工の計算機.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
