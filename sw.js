f8ffe7f1a12c302a31a65ffae70cdcc02a3ab71f
saku-0111 <yusaku1379@gmail.com>
2026-03-07 20:35:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daiku-calc-v1.001.002';
+const CACHE_NAME = 'daiku-calc-v2.1.1';
 const urlsToCache = [
   '大工の計算機.html'
 ];
@@ -16,4 +16,5 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
